30ffedde9e5602d23351a1819d19a7f5cfc6f59b
snakerabbit <alison.s.cheng@gmail.com>
2017-09-13 00:33:29 -0700

    W7D3 hw completed

---
+++ W7D3/components/giphys_search.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+    constructor() {
+      super();
+      this.state = {searchTerm: ""};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit= this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+      this.setState({searchTerm: e.currentTarget.value});
+    }
+    handleSubmit(e){
+      e.preventDefault();
+      let searchTerm = this.state.searchTerm.split(' ').join('+');
+      this.props.fetchSearchGiphys(searchTerm);
+    }
+
+    render() {
+      let {giphys} = this.props;
+      return (
+        <div>
+          <form className = "search-bar">
+            <input value = {this.state.searchTerm} onChange = {this.handleChange}/>
+            <button type = "submit" onClick = {this.handleSubmit}>Search</button>
+          </form>
+          <GiphysIndex giphys = {giphys}/>
+        </div>
+      );
+    }
+}
+export default GiphysSearch;
